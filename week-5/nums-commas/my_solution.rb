1f8d9b020080d23686600361703602340d5acbb0
tyler <tyler.mckenzie30@aol.com>
2015-12-11 15:44:34 +0000

    Add 5.5 for safety

---
+++ week-5/nums-commas/my_solution.rb
@@ -0,0 +1,45 @@
+# Numbers to Commas Solo Challenge
+
+# I spent [] hours on this challenge.
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# 0. Pseudocode
+
+# What is the input?
+# :The input will be and integer
+
+# What is the output? (i.e. What should the code return?)
+# :The output will be a string with the commas added
+
+# What are the steps needed to solve the problem?
+# the steps to solve will be the following:
+# def separate_comma(int)
+# 	convert the int to a string
+# 	determine how long the string is to be able to add appropriate commas
+# 	add the commas to the appropriate indexing
+#   display the adjusted string
+
+# 1. Initial Solution
+def separate_comma(int)
+  int_s = int.to_s
+  int_l = int_s.length
+  if int_l < 4 
+    int_s
+  elsif int_l >= 4 && int_l < 7
+    int_s.insert(-4, ',')
+  elsif int_l <= 8
+    int_s.insert(-4, ',')
+    int_s.insert(-8, ',')
+  end
+end
+
+
+# 2. Refactored Solution
+
+
+
+
+# 3. Reflection
